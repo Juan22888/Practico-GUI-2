7271843550aab37ab7457fb22780a8c0b703f856
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-09-01 18:35:44 -0300

    Creacion del proyecto en Maven

---
+++ PracticoGUI2/src/main/java/com/mycompany/practicogui2/PracticoGUI2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicogui2;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class PracticoGUI2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
