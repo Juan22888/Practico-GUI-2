25a2328f29855d4d78b0230e1dabedcd0c7b11ac
Barzola juan <barzolajuan661@gmail.com>
2025-09-03 19:44:50 -0300

    Arreglo de la clase Materia

--- PracticoGUI4/src/main/java/com/mycompany/practicogui2/Materia.java
+++ PracticoGUI4/src/main/java/com/mycompany/practicogui2/Materia.java
@@ -9,5 +9,41 @@ package com.mycompany.practicogui2;
  * @author LaMaquina
  */
 public class Materia {
+    private static int contador=0;
+    private int idMateria=0;
+    private String nombre;
+    private int anio;
+
+    public Materia( String nombre, int anio) {
+        this.idMateria = contador++;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
     
 }
